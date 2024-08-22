62bc96b05cf12b108bd1a611faf75ab4568ead1a
David Abram <david@crocoder.dev>
2024-08-22 14:41:15 +0000

    abc (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
+        <span>THIS SHOULD BE THE ULTIMATE COMMIT!</span>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -73,7 +74,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            ULTIMATE!
           </p>
         </a>
 
